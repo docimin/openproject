0659381819aa82798a6b14fbbdce0b61740c841b
ulferts <jens.ulferts@googlemail.com>
2026-03-18 17:41:14 +0100

    attempt to fix flickering spec on drag in sprint

--- modules/backlogs/spec/support/pages/backlogs.rb
+++ modules/backlogs/spec/support/pages/backlogs.rb
@@ -123,6 +123,8 @@ module Pages
                        end
 
       moved_element.native.drag_to(target_element.native, delay: 0.1)
+    rescue Capybara::Cuprite::ObsoleteNode
+      retry
     end
 
     def fold_backlog(backlog)
